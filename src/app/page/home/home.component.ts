46c3773799bfbd8ffae05abdba5feff8c64ca81e
RaiN <szabogya81@gmail.com>
2021-01-28 20:34:23 +0100

    Move top counts, imagepath to configservice

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../service/product.service';
+import { ConfigService } from 'src/app/service/config.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
   filterStr: string = '';
-  products = ProductService.getProducts(0, this.filterStr);
-  saleProducts = this.products.filter(p => p.salePrice).slice(0, ProductService.topSaleCount);
-  featuredProducts = this.products.filter(p => p.featured).slice(0, ProductService.topFeaturedCount);
+  products = this.service.getProducts(0, this.filterStr);
+  saleProducts = this.products.filter(p => p.salePrice).slice(0, this.config.topSaleCount);
+  featuredProducts = this.products.filter(p => p.featured).slice(0, this.config.topFeaturedCount);
   
-  constructor() { }
+  constructor(private config: ConfigService, private service: ProductService) { }
 
   ngOnInit(): void {
   }
