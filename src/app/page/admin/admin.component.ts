b9e6989c281ac391e67d208d4b00ac13f822259d
ethuringer <ethuringer@gmail.com>
2021-02-05 13:32:27 +0100

    data from product service is ready

--- src/app/page/admin/admin.component.ts
+++ src/app/page/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/service/config.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  filterStr: string = '';
+  products = this.service.getProducts(0, this.filterStr);
+
+  constructor(private config: ConfigService, private service: ProductService) { }
 
   ngOnInit(): void {
   }
