3390b8515d582286ba3af481fff0025ccbd26c46
Vilhelmus25 <vorvie@gmail.com>
2021-02-06 18:05:50 +0100

    extended-search

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -5,12 +5,41 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any[], filterStr: string): unknown {
- 
-    if (!Array.isArray(value) || !filterStr) {
+  transform(value: any[], filterStr: string, key: string = ''): unknown {
+
+    if (!Array.isArray(value) || !filterStr || !key) {
       return value;
     }
 
-    return value.filter(item => (item['name'].toLowerCase().includes(filterStr.toLowerCase())));
+    return value.filter(item => {
+
+      if (key === 'id') {
+        return (item[key].toString() === filterStr)
+      }
+      if (key === 'catId') {
+        return (item[key].toString() === filterStr);
+      }
+      if (key === 'name') {
+        return (item[key].toLowerCase().includes(filterStr.toLowerCase()))
+      }
+      if (key === 'description') {
+        return (item[key].toLowerCase().includes(filterStr.toLowerCase()))
+      }
+      if (key === 'image') {
+        return (item[key].toLowerCase().includes(filterStr.toLowerCase()))
+      }
+      if (key === 'price') {
+        return (item[key].toString() === filterStr)
+      }
+      if (key === 'stock') {
+        return (item[key].toString() === filterStr)
+      }
+      if (key === 'featured') {
+        return (item[key].toString() === filterStr)
+      }
+      if (key === 'active') {
+        return (item[key].toString() === filterStr)
+      }
+    });
   }
 }
